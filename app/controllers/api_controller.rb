4b4acb8711e2d4d4273a1c7a5e26e55ee4a5b2ee
cosmin-enache <cosminache96@gmail.com>
2021-03-04 14:50:53 +0200

    a

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,5 +1,6 @@
 class ApiController < ApplicationController
   skip_before_action :verify_authenticity_token
+  skip_before_filter :verify_authenticity_token
   before_action :check_api_key
   require 'json'
 
