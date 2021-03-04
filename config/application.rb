8124e6f7c803efe4dcbb2f2de3b2c2a2be1913bb
cosmin-enache <cosminache96@gmail.com>
2021-03-04 14:47:30 +0200

    Changes

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,5 @@ module SoundwaveApi
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    self.allow_forgery_protection = false
   end
 end
