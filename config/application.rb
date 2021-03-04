757383cfca121e5e019f664274d07c696511fe51
cosmin-enache <cosminache96@gmail.com>
2021-03-04 14:43:39 +0200

    Removed Setting

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,6 @@ module SoundwaveApi
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    self.allow_forgery_protection = false
   end
 end
